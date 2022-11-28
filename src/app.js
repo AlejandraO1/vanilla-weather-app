18658bd0216452f47f2bf4cff0c941e0bae2abc1
AlejandraO1 <alejandraortegax@gmail.com>
2022-11-28 16:58:44 -0500

    Updated date and time with javascript and in html

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
+function formatDateTime(timestamp) {
+  let currentDate = new Date(timestamp);
+  let hours = currentDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentDate.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayCurrentTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+  let dateTimeElement = document.querySelector("#date-time");
+  dateTimeElement.innerHTML = formatDateTime(response.data.time * 1000);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let descriptionElement = document.querySelector("#description");
