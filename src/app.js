90547d644a3cedd619b4a5f638a4aa31afea9619
AlejandraO1 <alejandraortegax@gmail.com>
2022-11-28 15:42:09 -0500

    Showing the current temperature in conjunction with the api

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 function displayCurrentTemperature(response) {
-  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "0d38oda45d824a5t0254fbaff5f740bd";
@@ -7,4 +16,5 @@ let urlEndpoint = "https://api.shecodes.io/weather/v1/current?";
 let query = "New York";
 let units = "imperial";
 let apiUrl = `${urlEndpoint}query=${query}&key=${apiKey}&units=${units}`;
+
 axios.get(apiUrl).then(displayCurrentTemperature);
